7f8712a43ea9ded5bf65981927e760f4eaad7b7e
Kevin Garcia <kg.gar91@gmail.com>
2020-06-15 21:03:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_oebject({}) do |(key,value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
+        binding.pry
         if !final_array[name]
           final_array[name] = {}
         end
