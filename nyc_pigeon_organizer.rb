eda35a1be098f9f92c90bf5554407416b533414c
Kevin Garcia <kg.gar91@gmail.com>
2020-06-15 20:54:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_oebject({}) do |(key,value), final_array|
+    binding.pry
+    final_array
   binding.pry
 end
